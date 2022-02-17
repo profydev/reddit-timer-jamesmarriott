f3ab4d4e6f4beb327fd635e52e0677c9b2112b90
James Marriott <sawheadproductions@gmail.com>
2022-02-17 14:39:15 +0100

    Added dependencies and setup routing (#1)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+        <Route path="/search" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
