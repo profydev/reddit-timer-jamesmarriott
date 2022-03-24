1a8062c5da2bdff16ee6e6fa0e71b0c6c8427fda
James Marriott <sawheadproductions@gmail.com>
2022-03-24 12:49:12 +0100

    fixed header alignment issues

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
-import Header from './Header';
-import GlobalStyle from './GlobalStyle';
+import Header from './components/header/Header';
+import GlobalStyle from './globalStyle';
 import theme from './theme';
 
 function App() {
--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+
+body {
+    letter-spacing: 1px;
+    font-family: 'Montserrat', sans-serif;
+    }
 `;
 
 export default GlobalStyle;
